9db92a0b58605ba33da1a24b261b000ebeb107f7
fdhlyna <128072211+suwwui@users.noreply.github.com>
2023-06-11 05:58:10 +0800

    first

---
+++ my-cv/src/App.js
@@ -0,0 +1,192 @@
+
+import './App.css';
+
+function Resume() {
+  return (
+        <div>
+          
+          <header className ="mx-3 py-3 block bg-white border-t" >
+            <span className="font-bold text-lime-700 items-center justify-center flex"> Nur Fadhlina Wjedi</span>
+
+          <div className="flex justify-center ">
+
+          <div className="items-center py-2 px-1 ">
+          <span class="text-sm">014-7740908 |</span>
+         </div>
+        
+          <div className="items-center py-2 px-1">
+          <span class="text-sm">Johor Bahru |</span>
+         </div>
+
+          <div className="items-center py-2 px-1">
+          <span class="text-sm">wfdhlyna@gmail.com |</span>
+         </div>
+
+          <div className=" flex items-center rounded-md py-2 px-1">
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="26" height="26" viewBox="0 0 48 48">
+            <path fill="#0288D1" d="M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5V37z"></path><path fill="#FFF" d="M12 19H17V36H12zM14.485 17h-.028C12.965 17 12 15.888 12 14.499 12 13.08 12.995 12 14.514 12c1.521 0 2.458 1.08 2.486 2.499C17 15.887 16.035 17 14.485 17zM36 36h-5v-9.099c0-2.198-1.225-3.698-3.192-3.698-1.501 0-2.313 1.012-2.707 1.99C24.957 25.543 25 26.511 25 27v9h-5V19h5v2.616C25.721 20.5 26.85 19 29.738 19c3.578 0 6.261 2.25 6.261 7.274L36 36 36 36z"></path>
+            </svg>
+          <span class="text-sm">Nur Fadhlina</span>
+        </div>
+
+        <div className=" flex items-center rounded-md py-2 px-1">
+        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="26" height="26" viewBox="0 0 64 64">
+          <path d="M32 6C17.641 6 6 17.641 6 32c0 12.277 8.512 22.56 19.955 25.286-.592-.141-1.179-.299-1.755-.479V50.85c0 0-.975.325-2.275.325-3.637 0-5.148-3.245-5.525-4.875-.229-.993-.827-1.934-1.469-2.509-.767-.684-1.126-.686-1.131-.92-.01-.491.658-.471.975-.471 1.625 0 2.857 1.729 3.429 2.623 1.417 2.207 2.938 2.577 3.721 2.577.975 0 1.817-.146 2.397-.426.268-1.888 1.108-3.57 2.478-4.774-6.097-1.219-10.4-4.716-10.4-10.4 0-2.928 1.175-5.619 3.133-7.792C19.333 23.641 19 22.494 19 20.625c0-1.235.086-2.751.65-4.225 0 0 3.708.026 7.205 3.338C28.469 19.268 30.196 19 32 19s3.531.268 5.145.738c3.497-3.312 7.205-3.338 7.205-3.338.567 1.474.65 2.99.65 4.225 0 2.015-.268 3.19-.432 3.697C46.466 26.475 47.6 29.124 47.6 32c0 5.684-4.303 9.181-10.4 10.4 1.628 1.43 2.6 3.513 2.6 5.85v8.557c-.576.181-1.162.338-1.755.479C49.488 54.56 58 44.277 58 32 58 17.641 46.359 6 32 6zM33.813 57.93C33.214 57.972 32.61 58 32 58 32.61 58 33.213 57.971 33.813 57.93zM37.786 57.346c-1.164.265-2.357.451-3.575.554C35.429 57.797 36.622 57.61 37.786 57.346zM32 58c-.61 0-1.214-.028-1.813-.07C30.787 57.971 31.39 58 32 58zM29.788 57.9c-1.217-.103-2.411-.289-3.574-.554C27.378 57.61 28.571 57.797 29.788 57.9z"></path>
+          </svg>
+          <span class="text-sm">suwwui</span>
+        </div>
+            
+        </div>
+            
+        </header>
+          
+   
+           <a href className="mx-3 block bg-white py-3 border-t">
+           <div className="flex justify-between">
+              <span className="font-bold text-lime-700">EDUCATION</span>
+            </div>
+            <span className=" text-sm font-semibold text-gray-900 ">Universiti Malaysia Sarawak, UNIMAS (2019-2022)</span>
+                <p className="text-sm font-semibold">Bachelor in Cognitive Science with Honors</p>
+                <div className="mx-3 text-justify">
+                <ul className="text-xs list-disc">
+                <li>3.64/4.0</li>
+                <li>MUET: Band 4</li>
+                <li>Sekretariat Sukan dan Rekreasi, Faculty of Cognitive Science and Human Development (2019-2020)</li>
+                </ul>
+              </div>
+  
+              <div className="py-3">
+                <span className="text-sm font-semibold">Pusat Pengajian Pra-Universiti, UNIMAS (2018-2019)</span>
+                <p className="text-sm font-semibold">Foundation in Life Science</p>
+              </div>
+            </a>
+
+          <a href className="mx-3 block bg-white py-3 border-t">
+           <div className="flex justify-between">
+              <span className="font-bold text-lime-700">WORKING EXPERIENCE</span>
+            </div>
+            <span className="text-sm font-semibold text-gray-900 ">Sales Assistant (Part Time)</span>
+                <p className="text-sm font-semibold">QBerries Bakery, Johor Bahru (Feb-Oct,2021)</p>
+                <div className="mx-3 text-justify">
+                <ul className=" text-xs list-disc">
+                <li>Maintained the store environment and replenished the store inventory supply with FIFO concept</li>
+                <li>Key selling products inside the POS system and filled documents related to payment receipt and invoice into labelled file</li>
+                <li>Quality checking for the store goods production and report back to the management</li>
+                </ul>
+              </div>
+              <div className="py-3">
+                <span className="text-sm font-semibold">UI/UX Designer Intern</span>
+                <p className="text-sm font-semibold">Imagine AI, Johor Bahru (July-Oct,2021)</p>
+                <div className="mx-3 text-justify">
+                <ul className="text-xs list-disc">
+                <li>Did market research on a software for pitching preparation for MRCB-ICP grant regarding the palm oil project.</li>
+                <li>Developed and conceptualized comprehensive UI/UX design strategy for the ongoing software projects of the company using Adobe XD and Illustrator</li>
+                <li>Collaborate with the marketing team and technical team to ensure the creation and delivery of tailored experiences for the digital user.</li>
+                </ul>
+                </div>
+              </div>
+              <div className="py-3">
+                <span className=" text-sm font-semibold">Software Developer Trainee</span>
+                <p className="text-sm font-semibold">Imagine AI, Johor Bahru (Aug-Sept,2022)</p>
+                <div className="mx-3 text-justify">
+                <ul className=" text-xs list-disc">
+                <li>Worked with other developers to design algorithms, interface and flowcharts for the proposed system based on client needs.</li>
+                <li>Gathered and evaluated user feedback by keeping in-touch with clients for updates during the system development and implementation.</li>
+                <li>Created technical documentation such hardware list, market research and quotation for reference and reporting.</li>
+                <li>Involved with the projects that utilized the React JS framework and standardized all proposed software interface by building GUI using Qt Designer</li>
+                </ul>
+                </div>
+              </div>
+              <div className="py-3">
+                <span className=" text-sm font-semibold">Operation Manager</span>
+                <p className="text-sm font-semibold">QBerries Bakery, Johor Bahru (Oct 2022-Feb 2023)</p>
+                <div className="mx-3 text-justify">
+                <ul className=" text-xs list-disc">
+                <li>Build the formula and standardized the sales monthly report with 2 store branches using MS Excel.</li>
+                <li>Developed, implemented, and reviewed operational policies and procedures such product SOP, Q&A customer booklet and staff training logbook</li>
+                <li>Oversees any issues and address the problem with the directors for problem solving approach</li>
+                </ul>
+                </div>
+              </div>
+          </a>
+
+          <a href className="mx-3 block bg-white py-3 border-t">
+           <div className="flex justify-between">
+              <span className="font-bold text-lime-700">ACTIVITIES AND INVOLVEMENT</span>
+            </div>
+
+            <span className="text-sm font-semibold text-gray-900 ">Softskills and Volunteerism, PPPU UNIMAS (2018)</span>
+            <ul className=" text-xs mx-3 list-disc">
+                <li>Supervised a community work in Kampung Muara Tuang, Kota Samarahan, Sarawak and assisted in postmortem for the cashflow tracking and evaluation process</li>
+            </ul>
+
+            <span className="text-sm font-semibold text-gray-900 ">4th place in Asasi Biology Colloquium, PPPU UNIMAS (2018)</span>
+            <ul className=" text-xs mx-3 list-disc">
+                <li>Collaborate in researching and analyzing of Pteridophytes plant species habitat at the Gunung Santubong Recreational Park</li>
+            </ul>
+
+            <span className="text-sm font-semibold text-gray-900 ">Citrawarna, UNIMAS (2019)</span>
+            <ul className=" text-xs mx-3 list-disc">
+                <li>Competed with 14 teams in dancing competition for Kolej Allamanda the start of Unimas’ 23rd Convocation Festival (Confest Unimas)</li>
+            </ul>
+
+            <span className="text-sm font-semibold text-gray-900 ">1st place in Linguistic PlayDay, Faculty of Cognitive Science and Human Development (2019)</span>
+            <ul className=" text-xs mx-3 list-disc">
+                <li>Workshop and competition to encourage student in designed a phonetics learning tools for assisting the introduction in production of speech sound by using Jenga blocks</li>
+            </ul>
+
+            <span className="text-sm font-semibold text-gray-900 ">1st place in Climate Change-Maker Challenge Hackathon, CreativeCulture UNIMAS (2022)</span>
+            <ul className=" text-xs mx-3 list-disc">
+                <li>Workshop and competition to educate the Kampung Pasir Pandak, Kuching community on the climate change and designed a pest detector by using the Arduino kit to replace the chemical repeller used during the food processing in the community</li>
+            </ul>
+            </a>
+
+          <a href className="mx-3 block bg-white py-3 border-t">
+           <div className="flex justify-between">
+              <span className="font-bold text-lime-700">PROJECT EXPERIENCE</span>
+            </div>
+
+            <span className="text-sm font-semibold text-gray-900 ">2018</span>
+            <ul className="text-xs mx-3 list-disc">
+                <li>Lead a group project to design and build a Coffee Vending Machine System for Basic Programming by using C++ language in Dev C++</li>
+            </ul>
+
+            <span className="text-sm font-semibold text-gray-900 ">2020</span>
+            <ul className="text-xs mx-3 list-disc">
+                <li>Lead a group project to prepare the feasibility and mock-up for Supervisee-supervisor System with Adobe XD in Expert System</li>
+            </ul>
+
+            <span className="text-sm font-semibold text-gray-900 ">2021</span>
+            <ul className="text-xs mx-3 list-disc">
+                <li>Lead group project to design and develop a functional circle detection software and the user interface with Octave in Computer Vision subject</li>
+            </ul>
+
+            <span className="text-sm font-semibold text-gray-900 ">2022</span>
+            <ul className="text-xs mx-3 list-disc">
+                <li>Lead group project to prepare the user specification and mock-up for Aturee System, a productivity application for student with Figma in Human Computer Interaction</li>
+                <li>Design a functional mobile application for TimeTable and Chatroom with Android Studio (Kotlin) and Firebase in Mobile Computing</li>
+                <li>Prepare and develop a functional web-based system for SME’s sales and stock management for Final Year Project called i-Log Inventory System</li>
+            </ul>
+            </a>
+
+          <a href className="mx-3 block bg-white py-3 border-t">
+           <div className="flex justify-between">
+              <span className="font-bold text-lime-700">SKILLS</span>
+            </div>
+            <ul className="mx-3 text-sm text-gray-900 ">
+              <li className="list-disc font-semibold">Programming Language:</li>
+              <p className="text-xs"> HTML, PHP, JavaScript, MySQL, Firebase, Kotlin, React JS, Tailwind, Octave, C++ </p>
+              <li className="list-disc font-semibold">Software: </li>
+              <p className="text-xs">Microsoft Word/Powerpoint/Excel/Access, Android Studio, GitHub, Arduino, Hostinger, WordPress, Figma, Adobe XD, Canva, Adobe Illustrator, RapidMiner </p>
+              <li className="list-disc font-semibold ">Language:</li> 
+              <p className="text-xs">English and Malay (Fluent)</p> 
+              </ul>
+            </a>
+
+          
+        </div>
+      );
+    };
+    
+  export default Resume;
+
